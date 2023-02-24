580c00ea79132cd8b8d5e9b475ef496a4c9abc00
rsek <r.sekouri@gmail.com>
2023-02-23 19:44:52 -0800

    config: siteUrl

--- iles.config.ts
+++ iles.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'iles'
 
 export default defineConfig({
+	siteUrl: 'https://rsek.github.io',
 	modules: ['@islands/headings'],
 	markdown: {
 		remarkPlugins: ['remark-gfm']
