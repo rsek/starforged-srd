5dfc541d9726dc95a47dbf22e5209232cdae0a63
rsek <5354757+rsek@users.noreply.github.com>
2023-02-24 16:28:40 -0800

    import kor ui css

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import { defineApp } from 'iles'
+import * as korStyles from '@kor-ui/kor/kor-styles.css'
 
-export default defineApp({ router: {} })
+export default defineApp({
+	head: {
+		link: [korStyles]
+	}
+})
